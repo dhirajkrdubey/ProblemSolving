1eeb74e05df0f7bf17a104ac47e0015f3de918a4
Dhiraj Dubey <dhirajdubey401@gmail.com>
2024-05-01 18:20:22 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (59.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        for(auto it : s){
+            if(it == '(' || it == '[' || it == '{')
+            st.push(it);
+            else{
+                if(st.empty()) return false;
+                char c = st.top();
+                st.pop();
+                if(it == ')' && c == '(' || it == ']' && c == '[' || it == '}' && c == '{')                    continue;
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
